d8aaa2b8e6e0c65844f69c6ab39c13135f233759
caokhacmanh <66982153+caokhacmanh@users.noreply.github.com>
2023-01-17 04:39:25 +0000

    Added template

---
+++ template.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// boost & macro
+#pragma optimize("O3")
+#define nmax 200005
+#define pint pair<int, int>
+#define pong pair<ll, ll>
+#define pool pair<bool, bool>
+#define vint vector<int>
+#define vong vector<ll>
+#define stkii stack<int>
+#define stkll stack<long long>
+#define fi first
+#define se second
+typedef string str;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef unsigned int uii;
+
+#define oo 1e18
+
+// necessary
+ll n = 0, m = 0, k = 0, t = 0, tmp = 0, ans = 0, run = 0, ins = 0;
+bool chk = 0;
+str s;
+ll rr[nmax];
+
+// optional
+
+
+// funcs
+
+
+int main(){
+	//freopen("inp.inp", "r", stdin);
+	//freopen("out.out", "w", stdout);
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	cin >> t;
+	while(t--){
+		
+
+
+	}
+}
+
+// COMMENT:
+
+/* <<<CODE EXPLANATION>>>
+
+
+
+*/
+
+
+/* <<<CRE>>>
+
+I, Sheyer, guaranteed that the idea for this code is personally made up by me: <Sheyer>
+In case it is not, I give credit to the original Author:
+
+I, Sheyer, guaranteed that this code is originally created by me, based on the idea mentioned above: <Sheyer>
+
+In belief of h1a4c3k, a long lost account
+
+*/
